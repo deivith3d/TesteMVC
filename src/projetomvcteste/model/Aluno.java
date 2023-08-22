f8647526039ded50eb06e1aec99bb7b404257032
deivith3d <deivith3d@gmail.com>
2023-08-22 00:41:10 -0300

    Classe Aluno

---
+++ src/projetomvcteste/model/Aluno.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetomvcteste.model;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dgcunha
+ */
+public class Aluno extends Pessoa{
+    private String curso; 
+    private int semestre;
+
+    public Aluno () {
+    }
+
+   
+}
